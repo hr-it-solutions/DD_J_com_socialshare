9dbb50804d6fd76aab9e6456a3d84e2415cd20f6
Florian Häusler <florian@didldu.com>
2017-10-02 16:30:12 +0200

    controller.php added default view

---
+++ admin/controller.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package    DD_SocialShare
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
+ * @copyright  Copyright (C) 2017 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+class DD_SocialShareController extends JControllerLegacy{
+
+	/**
+	 * The default view.
+	 *
+	 * @var    string
+	 *
+	 * @since  Version 1.0.0.0
+	 */
+	protected $default_view = 'dashboard';
+
+	/**
+	 * Method to display a view.
+	 *
+	 * @param   boolean        $cachable   If true, the view output will be cached
+	 * @param   array|boolean  $urlparams  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+	 *
+	 * @return  ContentController|boolean  This object to support chaining.
+	 *
+	 * @since   Version 1.0.0.0
+	 */
+	public function display($cachable = false, $urlparams = false)
+	{
+		return parent::display();
+	}
+}
