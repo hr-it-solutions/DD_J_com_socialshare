9c3d48105edfb11afb08da407d57a307a4a63fb3
Florian Häusler <florian@didldu.com>
2017-10-04 19:33:58 +0200

    added

---
+++ admin/controllers/article.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * @package    DD_SocialShare
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
+ * @copyright  Copyright (C) 2017 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+use Joomla\Utilities\ArrayHelper;
+
+class DD_SocialShareControllerArticle extends JControllerForm
+{
+	/**
+	 * Class constructor.
+	 *
+	 * @param   array  $config  A named array of configuration variables.
+	 *
+	 * @since    Version 1.0.0.0
+	 */
+	public function __construct($config = array())
+	{
+		parent::__construct($config);
+	}
+
+
+	/**
+	 * Method to add a new record.
+	 *
+	 * @return  boolean  True if the record can be added, false if not.
+	 *
+	 * @since   1.0.0.0
+	 */
+	public function add()
+	{
+		$context = "$this->option.edit.$this->context";
+
+		// Get associated content id
+		$content_id = $this->input->get('content_id', 0, 'int');
+
+		// Access check.
+		if ($content_id == 0 || !$this->allowAdd())
+		{
+			// Set the internal error and also the redirect error.
+			$this->setError(\JText::_('JLIB_APPLICATION_ERROR_CREATE_RECORD_NOT_PERMITTED'));
+			$this->setMessage($this->getError(), 'error');
+
+			$this->setRedirect(
+				\JRoute::_(
+					'index.php?option=' . $this->option . '&view=' . $this->view_list
+					. $this->getRedirectToListAppend(), false
+				)
+			);
+
+			return false;
+		}
+
+		// Clear the record edit information from the session.
+		\JFactory::getApplication()->setUserState($context . '.data', null);
+
+		// Redirect to the edit screen.
+		$this->setRedirect(
+			\JRoute::_(
+				'index.php?option=' . $this->option . '&view=' . $this->view_item
+				. $this->getRedirectToItemAppend() . '&conent_id=' . $content_id, false
+			)
+		);
+
+		return true;
+	}
+
+	/**
+	 * Method override to check if you can edit an existing record.
+	 *
+	 * @param   array   $data  An array of input data.
+	 * @param   string  $key   The name of the key for the primary key.
+	 *
+	 * @return  boolean
+	 *
+	 * @since    Version 1.0.0.0
+	 */
+	protected function allowEdit($data = array(), $key = 'id')
+	{
+		$recordId = (int) isset($data[$key]) ? $data[$key] : 0;
+		$user = JFactory::getUser();
+
+		// Zero record (id:0), return component edit permission by calling parent controller method
+		if (!$recordId)
+		{
+			return parent::allowEdit($data, $key);
+		}
+
+		// Check edit on the record asset (explicit or inherited)
+		if ($user->authorise('core.edit', 'com_dd_socialshare.article.' . $recordId))
+		{
+			return true;
+		}
+
+		// Check edit own on the record asset (explicit or inherited)
+		if ($user->authorise('core.edit.own', 'com_dd_socialshare.article.' . $recordId))
+		{
+			// Existing record already has an owner, get it
+			$record = $this->getModel()->getItem($recordId);
+
+			if (empty($record))
+			{
+				return false;
+			}
+
+			// Grant if current user is owner of the record
+			return $user->id == $record->created_by;
+		}
+
+		return false;
+	}
+}
