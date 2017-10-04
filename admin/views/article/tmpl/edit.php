11985dd6aa7d208f68d53df146aa8f6e880c454f
Florian Häusler <florian@didldu.com>
2017-10-05 01:27:21 +0200

    Revision

--- admin/views/article/tmpl/edit.php
+++ admin/views/article/tmpl/edit.php
@@ -16,115 +16,130 @@ JHtml::_('formbehavior.chosen', 'select');
 
 ?>
 <div id="dd_socialshare-article" class="row-fluid dd_socialshare">
-	<form action="<?php echo JRoute::_('index.php?option=com_dd_socialshare&layout=edit&id=' . (int) $this->item->id); ?>"
-	      method="post" name="adminForm" id="adminForm" class="form-validate">
-		<div class="row-fluid">
-			<div class="span12">
-				<div class="form-inline form-inline-header">
-					<div class="control-group">
-						<div class="control-label">
-							<?php echo $this->form->getLabel('title'); ?>
-						</div>
-						<div class="controls">
-							<?php echo $this->form->getInput('title'); ?>
-						</div>
-					</div>
-					<div class="control-group">
-						<div class="control-label">
-							<?php echo $this->form->getLabel('alias'); ?>
-						</div>
-						<div class="controls">
-							<?php echo $this->form->getInput('alias'); ?>
-						</div>
-					</div>
-                    <div class="control-group">
+<form action="<?php echo JRoute::_('index.php?option=com_dd_socialshare&layout=edit&id=' . (int) $this->item->id); ?>"
+      method="post" name="adminForm" id="adminForm" class="form-validate">
+<div class="row-fluid">
+    <div class="span12">
+        <div class="form-inline form-inline-header">
+            <div class="control-group">
+                <div class="control-label">
+                    <?php echo $this->form->getLabel('title'); ?>
+                </div>
+                <div class="controls">
+                    <?php echo $this->form->getInput('title'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+                    <?php echo $this->form->getLabel('alias'); ?>
+                </div>
+                <div class="controls">
+                    <?php echo $this->form->getInput('alias'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+                    <?php echo $this->form->getLabel('content_id'); ?>
+                </div>
+                <div class="controls">
+                    <?php echo $this->form->getInput('content_id'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+                    <?php echo $this->form->getLabel('id'); ?>
+                </div>
+                <div class="controls">
+                    <?php echo $this->form->getInput('id'); ?>
+                </div>
+            </div>
+        </div><hr>
+        <div class="row-fluid form-horizontal-desktop">
+            <div class="span6 well">
+                <h3><?php echo JText::_('COM_DD_SOCIALSHARE_FACEBOOK') . ' ' .  JText::_('COM_DD_SOCIALSHARE_PREVIEW'); ?>
+                    <span class="icon-twitter pull-right"></span>
+                </h3><hr>
+                <div class="row-fluid">
+                    <div class="span6 control-group">
                         <div class="control-label">
-							<?php echo $this->form->getLabel('content_id'); ?>
+                            <?php echo $this->form->getLabel('facebook_post_title'); ?>
                         </div>
                         <div class="controls">
-							<?php echo $this->form->getInput('content_id'); ?>
+                            <?php echo $this->form->getInput('facebook_post_title'); ?>
                         </div>
                     </div>
-                    <div class="control-group">
+                    <div class="span6 control-group">
                         <div class="control-label">
-							<?php echo $this->form->getLabel('id'); ?>
+                            <?php echo $this->form->getLabel('facebook_post_image'); ?>
                         </div>
                         <div class="controls">
-							<?php echo $this->form->getInput('id'); ?>
+                            <?php echo $this->form->getInput('facebook_post_image'); ?>
                         </div>
                     </div>
-				</div>
-                <hr>
-                <div class="row-fluid form-horizontal-desktop">
-                    <div class="span6 well">
-                        <h3>Facebook Preview <span class="icon-facebook pull-right"></span></h3>
-                        <hr>
-                        <div class="control-group">
-                            <div class="control-label">
-			                    <?php echo $this->form->getLabel('facebook_post_image'); ?>
-                            </div>
-                            <div class="controls">
-			                    <?php echo $this->form->getInput('facebook_post_image'); ?>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <div class="control-label">
-			                    <?php echo $this->form->getLabel('facebook_post_text'); ?>
-                            </div>
-                            <div class="controls">
-			                    <?php echo $this->form->getInput('facebook_post_text'); ?>
-                            </div>
-                        </div>
-
-                        <div class="facebook-success">
-                            <div class="alert alert-success">
-                                <h4>This Article is post on Facebook, see postdate below!</h4>
-                            </div>
-                            <div class="control-group">
-                                <div class="control-label">
-			                        <?php echo $this->form->getLabel('facebook'); ?>
-                                </div>
-                                <div class="controls">
-			                        <?php echo $this->form->getInput('facebook'); ?>
-                                </div>
-                            </div>
-                        </div>
-
-                        <button class="btn-facebook btn btn-large btn-success"><span class="icon-facebook"></span> Share now</button>
+                </div>
+                <div class="control-group">
+                    <div class="control-label">
+                        <?php echo $this->form->getLabel('facebook_post_url'); ?>
                     </div>
-                    <div class="span6 well">
-                        <h3>Twitter Preview <span class="icon-twitter pull-right"></span></h3>
-                        <hr>
-                        <div class="control-group">
-                            <div class="control-label">
-			                    <?php echo $this->form->getLabel('twitter_post_text'); ?>
-                            </div>
-                            <div class="controls">
-			                    <?php echo $this->form->getInput('twitter_post_text'); ?>
-                            </div>
-                        </div>
-
-                        <div class="twitter-success">
-                            <div class="alert alert-success">
-                                <h4>This Article is post on Twitter, see postdate below!</h4>
-                            </div>
-                            <div class="control-group">
-                                <div class="control-label">
-			                        <?php echo $this->form->getLabel('twitter'); ?>
-                                </div>
-                                <div class="controls">
-			                        <?php echo $this->form->getInput('twitter'); ?>
-                                </div>
-                            </div>
-                        </div>
-
-                        <button class="btn-twitter btn btn-large btn-success"><span class="icon-twitter"></span> Share now</button>
+                    <div class="controls">
+                        <?php echo $this->form->getInput('facebook_post_url'); ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="control-label">
+                        <?php echo $this->form->getLabel('facebook_post_text'); ?>
+                    </div>
+                    <div class="controls">
+                        <?php echo $this->form->getInput('facebook_post_text'); ?>
                     </div>
                 </div>
 
-				<input type="hidden" name="task" value=""/>
-				<?php echo JHtml::_('form.token'); ?>
-			</div>
-		</div>
-	</form>
+                <div class="facebook-success">
+                    <?php if ($this->item->facebook != NULL): ?>
+                    <div class="alert alert-success">
+                        <h4><?php echo JText::sprintf('COM_DD_SOCIALSHARE_ALREADYSHARED', JText::_('COM_DD_SOCIALSHARE_FACEBOOK')); ?></h4>
+                        <div class="control-group">
+                            <div class="control-label"><?php echo $this->form->getLabel('facebook'); ?></div>
+                            <div class="controls"><?php echo $this->form->getInput('facebook'); ?></div>
+                        </div>
+                    </div>
+                    <?php endif; ?>
+                </div>
+
+                <button class="btn-facebook btn btn-large btn-success"><span class="icon-facebook"></span> Share now</button>
+            </div>
+            <div class="span6 well">
+                <h3><?php echo JText::_('COM_DD_SOCIALSHARE_TWITTER') . ' ' . JText::_('COM_DD_SOCIALSHARE_PREVIEW'); ?>
+                    <span class="icon-twitter pull-right"></span>
+                </h3><hr>
+                <div class="control-group">
+                    <div class="control-label">
+                        <?php echo $this->form->getLabel('twitter_post_text'); ?>
+                    </div>
+                    <div class="controls">
+                        <?php echo $this->form->getInput('twitter_post_text'); ?>
+                    </div>
+                </div>
+
+                <div class="twitter-success">
+                    <?php if ($this->item->twitter != NULL): ?>
+                    <div class="alert alert-success">
+                        <h4><?php echo JText::sprintf('COM_DD_SOCIALSHARE_ALREADYSHARED', JText::_('COM_DD_SOCIALSHARE_TWITTER')); ?></h4>
+                        <div class="control-group">
+                            <div class="control-label"><?php echo $this->form->getLabel('twitter'); ?></div>
+                            <div class="controls"><?php echo $this->form->getInput('twitter'); ?></div>
+                        </div>
+                    </div>
+                    <?php endif; ?>
+                </div>
+
+                <button class="btn-twitter btn btn-large btn-success"><span class="icon-twitter"></span> Share now</button>
+            </div>
+        </div>
+
+        <input type="hidden" name="task" value=""/>
+        <?php echo JHtml::_('form.token'); ?>
+    </div>
+</div>
+</form>
 </div>
